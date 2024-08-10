0ecf4c54612428d960f9b6ee266bae7ecad0d31a
Angoliuk <gamongara@gmail.com>
2024-08-10 15:10:19 +0300

    pwd

--- cpack/cpack.cmake
+++ cpack/cpack.cmake
@@ -1,5 +1,5 @@
 set(CPACK_GENERATOR "ZIP")
-set(CPACK_PACKAGE_NAME "wind-core")
+set(CPACK_PACKAGE_FILE_NAME "wind-core")
 
 set(CPACK_SOURCE_IGNORE_FILES
   "/build/;/.git/;/.vscode/;/CMakeFiles/;/CMakeCache.txt;/Makefile;/cmake_install.cmake"
