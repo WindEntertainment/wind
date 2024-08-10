24b760b8f8ca2b80da016432182ba99119277ee8
Angoliuk <gamongara@gmail.com>
2024-08-10 14:59:10 +0300

    test

--- cpack/cpack.cmake
+++ cpack/cpack.cmake
@@ -1,16 +1,13 @@
-set(CPACK_BUNDLE_NAME "Dreich")
-set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Dreich Inc")
-set(CPACK_PACKAGE_FILE_NAME "dreich")
-
-set_target_properties(game PROPERTIES INSTALL_RPATH "@executable_path")
-
-install (TARGETS game DESTINATION .)
-
-install (DIRECTORY $ENV{ULTRALIGHT_PATH}/bin/ DESTINATION .)
-
-install (DIRECTORY game/res DESTINATION .)
-
 set(CPACK_GENERATOR "ZIP")
-set(CPACK_BUNDLE_STARTUP_COMMAND ${CMAKE_BINARY_DIR}/game/game)
+set(CPACK_PACKAGE_NAME "wind-core")
+
+set(CPACK_SOURCE_IGNORE_FILES
+  "/build/;/.git/;/.vscode/;/CMakeFiles/;/CMakeCache.txt;/Makefile;/cmake_install.cmake"
+)
+
+
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/wind/core/" DESTINATION ".")
+
+message("Here: ${CMAKE_SOURCE_DIR}")
 
 include(CPack)
